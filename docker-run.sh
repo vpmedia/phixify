5ba6641ef9ad9820554a40b48fcb0235b4aa30d9
andras-macbook-air <andras@vpmedia.hu>
2022-11-03 19:38:15 +0100

    Updated docker helper

--- docker-run.sh
+++ docker-run.sh
@@ -13,4 +13,4 @@ docker build -t phixify:latest . && \
 rm -rf ./asset && \
 mkdir -p ./asset && \
 rsync -a --include '*/' --include '*.png' --include '*.wav' --exclude '*' "$(pwd)"/../phixify-example/public/asset/single_project_single_bundle/ ./asset/ && \
-docker run --mount type=bind,source="$(pwd)"/asset,target=/asset phixify:latest ./phixify.sh all --verbose
+docker run --mount type=bind,source="$(pwd)"/asset,target=/phixify/asset phixify:latest ./phixify.sh all --verbose
