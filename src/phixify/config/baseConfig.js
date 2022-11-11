aa226ad649cb733a1edae21dfb67a14ed672f44e
Andras Csizmadia <andras@vpmedia.hu>
2022-11-11 20:39:16 +0100

    Release v1.0.7

--- src/phixify/config/baseConfig.js
+++ src/phixify/config/baseConfig.js
@@ -113,7 +113,7 @@ export const baseConfig = {
   phixify: {
     name: "@vpmedia/phixify",
     url: "https://github.com/vpmedia/phixify",
-    version: "1.0.6",
+    version: "1.0.7",
     copyright: "Copyright (c) Andras Csizmadia <andras@vpmedia.hu> (www.vpmedia.hu)",
     timestamp: new Date().getTime().toString(),
   },
