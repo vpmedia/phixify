33e934d9da03094ebb347b2ab502b9f7698224dd
Andras Csizmadia <andras@vpmedia.hu>
2023-10-09 10:19:40 +0200

    Fixed rollup config issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import { readFileSync } from 'node:fs';
 
 // Use import.meta.url to make the path relative to the current source file instead of process.cwd()
@@ -33,6 +34,6 @@ export default {
       exports: 'named',
     },
   ],
-  external: Object.keys(pkg.dependencies),
-  plugins: [nodeResolve(), commonjs()],
+  external: ['/node_modules/'],
+  plugins: [nodeResolve(), commonjs(), json()],
 };
