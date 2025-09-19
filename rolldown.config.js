1551dd2823dd273bd3e3b2715e078ae15a93aef6
Andras Csizmadia <andras@vpmedia.hu>
2025-09-19 10:52:01 +0200

    Migrated from rollup to rolldown (rs based bundler)

---
+++ rolldown.config.js
@@ -0,0 +1,40 @@
+import { readFileSync } from 'node:fs';
+import { defineConfig } from 'rolldown';
+
+// Use import.meta.url to make the path relative to the current source file instead of process.cwd()
+const packageFile = readFileSync(new URL('./package.json', import.meta.url));
+const pkg = JSON.parse(packageFile.toString());
+
+const projectName = 'phixify';
+const compiled = new Date().toUTCString().replace(/GMT/g, 'UTC');
+
+const banner = [
+  `#!/usr/bin/env node`,
+  `/**`,
+  ` * ${pkg.name}`,
+  ` * @description ${pkg.description}`,
+  ` * @author ${pkg.author}`,
+  ` * @version ${pkg.version}`,
+  ` * @license ${pkg.license}`,
+  ` * @see ${pkg.homepage}`,
+  ` * @generated ${compiled}`,
+  ` */`,
+].join('\n');
+
+const external = Object.keys(pkg.dependencies);
+external.push('node_modules');
+
+export default defineConfig({
+  platform: 'node',
+  input: 'src/index.js',
+  output: [
+    {
+      banner,
+      name: pkg.name,
+      file: `dist/${projectName}.js`,
+      format: 'es',
+      exports: 'named',
+    },
+  ],
+  external,
+});
