7b855fd496ca369e505f5a8a78b26f4078f3e318
Andras Csizmadia <andras@vpmedia.hu>
2025-09-22 09:31:31 +0200

    Updated vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true, // allows using describe/test/expect without imports
+    globals: true,
     environment: 'node', // 'node' or 'jsdom'
     // setupFiles: ['./src/lib/test/core.js'],
     watch: false,
     coverage: {
-      provider: 'v8', // 'v8' or 'istanbul'
-      reporter: ['text', 'html'],
+      provider: 'v8',
+      reporter: ['text'],
     },
   },
 });
