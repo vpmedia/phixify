6b3314c9057a831352f4e7d64119cb14bbf4f992
Andras Csizmadia <andras@vpmedia.hu>
2026-01-21 08:05:04 +0100

    test: optimize vitest performance, use standard config format

--- vitest.config.js
+++ vitest.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    environment: 'node',
     globals: true,
-    environment: 'node', // 'node' or 'jsdom'
-    // setupFiles: ['./src/lib/test/core.js'],
+    include: ['./src/**/*.test.{js,jsx,ts,tsx}'],
+    isolate: false,
+    pool: 'threads',
     watch: false,
     coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
       provider: 'v8',
       reporter: ['text'],
     },
